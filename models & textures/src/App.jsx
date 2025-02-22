e8f8ede6a446cb9510fd6dcbf7403cdc7218fb8b
vipin018 <vipinpathak018@gmail.com>
2025-02-22 21:09:51 +0530

    updated

--- models & textures/src/App.jsx
+++ models & textures/src/App.jsx
@@ -1,17 +1,19 @@
 import { Canvas } from '@react-three/fiber'
 import React from 'react'
-import { OrbitControls } from '@react-three/drei'
+// import { OrbitControls } from '@react-three/drei'
 import { Experience } from './components/Experience'
 import { Environment } from '@react-three/drei'
 const App = () => {
   return (
-    
+    < >
+    <h1 style={{
+      fontSize: '30rem', textAlign: 'center', position: 'absolute', top: '5%', left: '50%', transform: 'translate(-50%, -50%)', fontFamily: 'system-ui',}}>CUBE</h1>
     <Canvas camera={{position: [0, 0, 10], fov: 45}}>
-      <OrbitControls />
+      {/* <OrbitControls /> */}
       <Experience />
       <Environment preset="sunset" /> 
     </Canvas>
-    
+    </>
   )
 }
 
--- models & textures/src/components/Experience.jsx
+++ models & textures/src/components/Experience.jsx
@@ -1,37 +1,27 @@
-import { Fish } from "./Fish"
-import { useTexture, useVideoTexture } from "@react-three/drei"
+import { useRef, useState } from "react";
+import { useVideoTexture } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 
 export const Experience = () => {
-    // const woodenTexture = useTexture("/textures/wooden.jpg");
-    // const roughTexture = useTexture("/textures/roughness.jpg");
-    // const metalTexture = useTexture("/textures/metal.jpg");
-    // const plasticTexture = useTexture("/textures/plastic.webp");
     const videoTexture = useVideoTexture("/textures/stars.mp4");
-    // const paperTexture = useVideoTexture("/textures/newspaper.mp4");
     
+    // Reference for the cube
+    const cubeRef = useRef();
+
+    // UseFrame for smooth forward & backward rotation
+    useFrame((state, delta) => {
+        if (cubeRef.current) {
+            cubeRef.current.rotation.x = Math.sin(state.clock.elapsedTime) * 1.5; // Oscillates back and forth
+            cubeRef.current.rotation.y += delta * 0.9; // Rotates in Y axis
+        }
+    });
 
     return (
         <>
-            {/* <ambientLight intensity={3} /> */}
-            {/* <Fish position={[0, -2, 0]} scale={1.5} /> */}
-            <mesh position={[0, 0, 0]}>
+            <mesh ref={cubeRef} position={[0, 0, 0]}>
                 <boxGeometry args={[3, 3, 3]} />
-                <meshStandardMaterial  map={videoTexture} />
+                <meshStandardMaterial map={videoTexture} />
             </mesh>
-            {/* <mesh position={[0, 0, 0]} >
-                <sphereGeometry args={[1.5, 32, 32]} />
-                <meshStandardMaterial transparent opacity={1} map={paperTexture} />
-            </mesh>
-            <mesh position={[-3, 0, 0]}>
-                <torusKnotGeometry args={[1, 0.3, 100, 16]} />
-                <meshStandardMaterial transparent opacity={0} map={metalTexture} />
-            </mesh>
-            <mesh position={[0, -3, 0]}>
-                <cylinderGeometry args={[1, 1, 2, 32]} />
-                <meshStandardMaterial transparent opacity={0} map={roughTexture} />
-                </mesh> */}
-      
         </>
-    )
-}
-
+    );
+};
