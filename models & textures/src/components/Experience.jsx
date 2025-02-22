de78c7a39126545013ede3019fd69a9011fe3071
vipin018 <vipinpathak018@gmail.com>
2025-02-22 19:14:17 +0530

    updated

--- models & textures/src/components/Experience.jsx
+++ models & textures/src/components/Experience.jsx
@@ -1,30 +1,24 @@
 import { Fish } from "./Fish"
 import { useTexture, useVideoTexture } from "@react-three/drei"
-import { useEffect } from "react"
-import { RepeatWrapping } from "three"
+
 export const Experience = () => {
-    const woodenTexture = useTexture("/textures/wooden.jpg");
-    const roughTexture = useTexture("/textures/roughness.jpg");
-    const metalTexture = useTexture("/textures/metal.jpg");
-    const plasticTexture = useTexture("/textures/plastic.webp");
+    // const woodenTexture = useTexture("/textures/wooden.jpg");
+    // const roughTexture = useTexture("/textures/roughness.jpg");
+    // const metalTexture = useTexture("/textures/metal.jpg");
+    // const plasticTexture = useTexture("/textures/plastic.webp");
     const videoTexture = useVideoTexture("/textures/stars.mp4");
-    const paperTexture = useVideoTexture("/textures/newspaper.mp4");
-    useEffect(() => {
-        paperTexture.repeat.set(1, 1)
-        paperTexture.wrapS = RepeatWrapping
-        paperTexture.wrapT = RepeatWrapping
-    }, [])
+    // const paperTexture = useVideoTexture("/textures/newspaper.mp4");
+    
 
     return (
         <>
             {/* <ambientLight intensity={3} /> */}
             {/* <Fish position={[0, -2, 0]} scale={1.5} /> */}
-            <group position={[0, 1, 0]}>
-            <mesh position={[3, 0, 0]}>
-                <boxGeometry args={[2, 2, 2]} />
-                <meshStandardMaterial map={videoTexture} />
+            <mesh position={[0, 0, 0]}>
+                <boxGeometry args={[3, 3, 3]} />
+                <meshStandardMaterial  map={videoTexture} />
             </mesh>
-            <mesh position={[0, 0, 0]} >
+            {/* <mesh position={[0, 0, 0]} >
                 <sphereGeometry args={[1.5, 32, 32]} />
                 <meshStandardMaterial transparent opacity={1} map={paperTexture} />
             </mesh>
@@ -35,8 +29,8 @@ export const Experience = () => {
             <mesh position={[0, -3, 0]}>
                 <cylinderGeometry args={[1, 1, 2, 32]} />
                 <meshStandardMaterial transparent opacity={0} map={roughTexture} />
-                </mesh>
-            </group>
+                </mesh> */}
+      
         </>
     )
 }
