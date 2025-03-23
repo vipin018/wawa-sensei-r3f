0a97b891a6ecf863d44dca2d1812e08593ef28bf
vipin018 <vipinpathak018@gmail.com>
2025-03-23 23:27:19 +0530

    updated

--- r3f-hooks/src/Plane.jsx
+++ r3f-hooks/src/Plane.jsx
@@ -5,7 +5,7 @@ const Plane = () => {
         
         <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
             <planeGeometry args={[10, 10]} />
-            <meshStandardMaterial color={"white"} />
+            <meshStandardMaterial color={"black"} metalness={0.3} roughness={0.2} />
         </mesh>
     )
 }
