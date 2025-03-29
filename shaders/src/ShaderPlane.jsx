a8ac200ad192787c1c4006b0b4b9957fa579713d
vipin018 <vipinpathak018@gmail.com>
2025-03-29 11:01:04 +0530

    updated

--- shaders/src/ShaderPlane.jsx
+++ shaders/src/ShaderPlane.jsx
@@ -1,8 +1,22 @@
+
+import { shaderMaterial } from "@react-three/drei";
+import { extend } from "@react-three/fiber";
+import vertexShader from "./shaders/vertex.glsl";
+import fragmentShader from "./shaders/fragment.glsl";
+
+const MyShaderMaterial = shaderMaterial(
+  {},
+  vertexShader,
+  fragmentShader
+);
+
+extend({ MyShaderMaterial });
+
 export const ShaderPlane = ({ ...props }) => {
   return (
     <mesh {...props}>
       <planeGeometry args={[1, 1]} />
-      <meshBasicMaterial color={"black"} />
+        <myShaderMaterial/>
     </mesh>
   );
 };
